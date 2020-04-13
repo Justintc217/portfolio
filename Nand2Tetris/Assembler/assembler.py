4b19ac681be02cfc1442ae124f38ba79adfa89d4
Justintc217 <jutnlife@gmail.com>
2020-04-12 21:30:11 -0700

    refactoring Assembler to improve immutability

--- Nand2Tetris/Assembler/assembler.py
+++ Nand2Tetris/Assembler/assembler.py
@@ -1,71 +1,96 @@
 from functools import reduce
-from c_instruction_map import comp, dest, jump, symbols
 
-def assembler(hack_assembly): # returns map
-    assembly_with_ref = list(filter(None, hack_assembly))
-    reference_finder(assembly_with_ref)
-    assembly = filter(
-        lambda x: not x.startswith("("),
-        assembly_with_ref
-        )
-    return reduce(
-        (lambda x, y: x + y),
-        map(
-            decider,
+class Assembler():
+    def __init__(self, comp, dest, jump, symbols, asm_file):
+        self.comp = comp
+        self.dest = dest
+        self.jump = jump
+        self.symbols = symbols
+        self.asm_file = asm_file
+    
+    # prepare data
+    def extract_assembly_from_file(self):
+        def clean_line(line):
+            return line.rstrip("\n").replace(" ", "").split("//")[0]
+        cleaned_assembly = [clean_line(line) for line in self.asm_file.readlines()]
+        filtered_assembly = list(filter(None, cleaned_assembly))
+        return filtered_assembly
+    
+    def load_reference_to_symbols_table(self, assembly):
+        # side effect
+        index = 0
+        for line in assembly:
+            if line.startswith("("):
+                ref = line.lstrip("(").rstrip(")")
+                self.symbols[ref] = str(index)
+            else:
+                index += 1
+    
+    def remove_references(self, assembly):
+        assembly_without_references = list(filter(
+            lambda x: not x.startswith("("),
             assembly
+            ))
+        return assembly_without_references
+    
+    # assembly -> binary
+    def assemble(self, assembly_without_references):
+        binary_output = reduce(
+            (lambda x, y: "%s%s" %(x,y)),
+            map(
+                self.decider,
+                assembly_without_references
+                )
             )
-    )
+        return binary_output
 
-def reference_finder(assembly_with_ref):
-    index = 0
-    for line in assembly_with_ref:
-        if line.startswith("("):
-            ref = line.lstrip("(").rstrip(")")
-            symbols[ref] = index
-        else:
-            index += 1
+    def decider(self, line):
+        if line.startswith("@"):
+            return self.A_handler(line) + "\n"
+        return self.C_handler(line) + "\n"
 
-def decider(line):
-    if line.startswith("@"):
-        return A_handler(line) + "\n"
-    return C_handler(line) + "\n"
-
-# A handling
-def symbol_to_value(symbolic_address):
-    try:
-        symbolic_address = symbols[symbolic_address]
-    except KeyError:
+    # A instruction handling
+    def A_handler(self, line):
+        address = line.lstrip("@")
+        try:
+            address = int(address)
+        except ValueError:
+            address = self.symbolic_address_to_address(address)
+        binary = '{:016b}'.format(address)
+        return binary
+    
+    def symbolic_address_to_address(self, symbolic_address):
+        # A_handler helper function 
+        try:
+            address = int(self.symbols[symbolic_address])
+        except KeyError:
+            self.add_symbol_address(symbolic_address)
+            address = int(self.symbols[symbolic_address])
+        return address
+    
+    def load_symbol_address_to_symbols_table(self, symbolic_address):
+        # side effect of the A_handler helper
         for open_address in range(16, 16384):
-            if open_address not in symbols.values():
-                symbols[symbolic_address] = open_address
-                symbolic_address = open_address
+            if open_address not in self.symbols.values():
+                self.symbols[symbolic_address] = str(open_address)
                 break
-    return symbolic_address
-
-def A_handler(line):
-    address = line.lstrip("@")
-    try:
-        address = int(address)
-    except ValueError:
-        address = int(symbol_to_value(address))
-    binary = '{:016b}'.format(address)
-    return binary
-
-# C handling
-def partition(line):
-    if "=" in line:
-        dest_key, comp_and_jump = line.split("=")
-    else:
-        comp_and_jump = line
-        dest_key = "null"
-    if ";" in line:
-        comp_key, jump_key = comp_and_jump.split(";")
-    else:
-        comp_key = comp_and_jump
-        jump_key = "null"
-    return comp_key, dest_key, jump_key
-
-def C_handler(line):
-    comp_key, dest_key, jump_key = partition(line)
-    binary = "111" + comp[comp_key] + dest[dest_key] + jump[jump_key]
-    return binary
+    
+    # C instruction handling
+    def C_handler(self, line):
+        comp_key, dest_key, jump_key = self.partition(line)
+        binary = "111" + self.comp[comp_key] + self.dest[dest_key] + self.jump[jump_key]
+        return binary    
+    
+    def partition(self, line):
+        # C_handler helper function
+        if "=" in line:
+            dest_key, comp_and_jump = line.split("=")
+        else:
+            comp_and_jump = line
+            dest_key = "null"
+        if ";" in line:
+            comp_key, jump_key = comp_and_jump.split(";")
+        else:
+            comp_key = comp_and_jump
+            jump_key = "null"
+        return comp_key, dest_key, jump_key
--- Nand2Tetris/Assembler/main.py
+++ Nand2Tetris/Assembler/main.py
@@ -1,6 +1,7 @@
 import sys
 from pathlib import Path
-from assembler import assembler
+from Assembler import Assembler
+from c_instruction_map import comp, dest, jump, symbols
 
 try:
     relative_path = Path(sys.argv[1])
@@ -13,12 +14,14 @@ HACK_PATH = PROGRAM_PATH.parent / (PROGRAM_PATH.stem + ".hack")
 
 if __name__ == "__main__":
     with open(PROGRAM_PATH) as asm_file:
-        hack_assembly = [
-            line.rstrip("\n").replace(" ", "").split("//")[0] for line in asm_file.readlines()
-        ]
+        assembler_instance = Assembler(comp, dest, jump, symbols, asm_file)
+        assembly = assembler_instance.extract_assembly_from_file()
 
-    hack_binary = assembler(hack_assembly)
+    assembler_instance.load_reference_to_symbols_table(assembly)
+
+    assembly_without_references = assembler_instance.remove_references(assembly)
+
+    hack_binary = assembler_instance.assemble(assembly_without_references)
 
     with open(HACK_PATH, "w") as hack_file:
         hack_file.writelines(hack_binary)
-        hack_file.close()
